729640fbfa0128e241274a6a2bfb8ff49a877d56
yrodriguezle <yrodriguezle@gamil.com>
2025-03-18 17:04:33 +0100

    remove backend

--- backend/Models/Common/TokenResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace duedgusto.Models.Common;
-
-public class TokenResponse(string token, string refreshToken)
-{
-    public string Token { get; set; } = token;
-    public string RefreshToken { get; set; } = refreshToken;
-}
--- backend/Models/Common/UserContext.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Security.Claims;
-
-namespace duedgusto.Models;
-
-public class UserContext(ClaimsPrincipal principal) : Dictionary<string, object?>
-{
-    public required ClaimsPrincipal ClaimsPrincipal { get; set; } = principal;
-}
