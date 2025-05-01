9ef293bd9aecc28ac75a689411d1ee22ac6c5879
Yalian Rodriguez Leyva <yrodriguezle@gmail.com>
2025-05-01 20:38:19 +0200

    fix: scrollable-box

--- duedgusto/src/components/pages/roles/RoleDetails.tsx
+++ duedgusto/src/components/pages/roles/RoleDetails.tsx
@@ -109,7 +109,7 @@ function RoleDetails() {
     }
   };
 
-  
+
   return (
     <Formik
       innerRef={formRef}
@@ -130,7 +130,7 @@ function RoleDetails() {
           <FormikToolbar
             onFormReset={handleResetForm}
           />
-          <Box className="scrollable-box" sx={{ marginTop: 1, paddingX: 2, overflow: 'auto', height: 'calc(100vh - 64px - 38px)' }}>
+          <Box className="scrollable-box" sx={{ marginTop: 1, paddingX: 2, overflow: 'auto', height: 'calc(100vh - 64px - 41px)' }}>
             <Typography id="view-title" variant="h5" gutterBottom>
               {title}
             </Typography>
