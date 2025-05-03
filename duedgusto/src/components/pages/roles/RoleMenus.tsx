d3c20e60e47af8cafec8dbbc85fd404786a5d270
Yalian Rodriguez Leyva <yrodriguezle@gmail.com>
2025-05-03 18:40:15 +0200

    fix: opened groups

--- duedgusto/src/components/pages/roles/RoleMenus.tsx
+++ duedgusto/src/components/pages/roles/RoleMenus.tsx
@@ -1,8 +1,8 @@
 
-import { useEffect, useMemo } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useFormikContext } from "formik";
 import { Box } from "@mui/material";
-import { GridReadyEvent } from "ag-grid-community";
+import { GridReadyEvent, RowGroupOpenedEvent } from "ag-grid-community";
 import AgGrid from "../../common/datagrid/AgGrid";
 import { MenuNonNull } from "../../common/form/searchbox/searchboxOptions/menuSearchboxOptions";
 
@@ -12,38 +12,50 @@ interface RoleMenusProps {
 }
 
 function RoleMenus({ menus, onGridReady }: RoleMenusProps) {
+  const [opened, setOpened] = useState('');
   const { status } = useFormikContext();
   const readOnly = useMemo(() => status.isFormLocked as boolean, [status.isFormLocked]);
 
   useEffect(() => {
-    if (readOnly) {
-      const containers = document.querySelectorAll('div[role="presentation"][data-ref="eCheckbox"].ag-labeled.ag-label-align-right.ag-checkbox.ag-input-field:not(.ag-disabled)');
-      containers.forEach(container => {
-        container.classList.add('ag-disabled');
-        container.setAttribute('data-selection', 'true');
+    setTimeout(() => {
+      if (readOnly) {
+        const containers = document.querySelectorAll('div[role="presentation"][data-ref="eCheckbox"].ag-labeled.ag-label-align-right.ag-checkbox.ag-input-field:not(.ag-disabled)');
+        containers.forEach(container => {
+          container.classList.add('ag-disabled');
+          container.setAttribute('data-selection', 'true');
 
-        const wrapper = container.querySelector('.ag-wrapper.ag-input-wrapper.ag-checkbox-input-wrapper');
-        const checkbox = wrapper?.querySelector('input[type="checkbox"]');
-        if (wrapper && checkbox && checkbox instanceof HTMLInputElement) {
-          wrapper.classList.add('ag-disabled');
-          checkbox.disabled = true;
-        }
-      });
-    } else {
-      const containers = document.querySelectorAll('div[data-selection="true"].ag-disabled');
-      containers.forEach(container => {
-        container.classList.remove('ag-disabled');
-        container.removeAttribute('data-selection');
+          const wrapper = container.querySelector('.ag-wrapper.ag-input-wrapper.ag-checkbox-input-wrapper');
+          const checkbox = wrapper?.querySelector('input[type="checkbox"]');
+          if (wrapper && checkbox && checkbox instanceof HTMLInputElement) {
+            wrapper.classList.add('ag-disabled');
+            checkbox.disabled = true;
+          }
+        });
+      } else {
+        const containers = document.querySelectorAll('div[data-selection="true"].ag-disabled');
+        containers.forEach(container => {
+          container.classList.remove('ag-disabled');
+          container.removeAttribute('data-selection');
 
-        const wrapper = container.querySelector('.ag-wrapper.ag-input-wrapper.ag-checkbox-input-wrapper');
-        const checkbox = wrapper?.querySelector('input[type="checkbox"]');
-        if (wrapper && checkbox && checkbox instanceof HTMLInputElement) {
-          wrapper.classList.remove('ag-disabled');
-          checkbox.disabled = false;
-        }
-      });
-    }
-  }, [readOnly]);
+          const wrapper = container.querySelector('.ag-wrapper.ag-input-wrapper.ag-checkbox-input-wrapper');
+          const checkbox = wrapper?.querySelector('input[type="checkbox"]');
+          if (wrapper && checkbox && checkbox instanceof HTMLInputElement) {
+            wrapper.classList.remove('ag-disabled');
+            checkbox.disabled = false;
+          }
+        });
+      }
+    }, 0)
+  }, [readOnly, opened]);
+
+  const onRowGroupOpened = useCallback(
+    (event: RowGroupOpenedEvent) => {
+      const node = event.node;
+      const nodeString = `${node.id} - ${node.group} - ${node.expanded}`;
+      setOpened(nodeString);
+    },
+    [],
+  );
 
   return (
     <Box sx={{ marginTop: 1, paddingX: 1, height: '60vh' }}>
@@ -53,14 +65,22 @@ function RoleMenus({ menus, onGridReady }: RoleMenusProps) {
         treeData
         treeDataParentIdField="parentMenuId"
         groupDefaultExpanded={-1}
+        groupLockGroupColumns={-1}
         autoGroupColumnDef={{
           headerName: 'Titolo',
           field: 'title',
           cellRenderer: 'agGroupCellRenderer',
+          cellRendererParams: {
+            // disables ↵ Enter to expand/collapse
+            suppressEnterExpand: true,
+            // disables double-click to expand/collapse
+            suppressDoubleClickExpand: true,
+          },
           filter: true,
           sortable: true,
           width: 200,
         }}
+        onRowGroupOpened={onRowGroupOpened}
         columnDefs={[
           { headerName: 'Icona', field: 'icon', filter: true, sortable: true, width: 150 },
           { headerName: 'View', field: 'viewName', filter: true, sortable: true, width: 150 },
--- duedgusto/src/store/themeStore.tsx
+++ duedgusto/src/store/themeStore.tsx
@@ -12,8 +12,10 @@ function themeStore(set: StoreSet) {
   return {
     userTheme: defaultUserTheme(),
     changeTheme: (theme: ThemeMode) => {
+      const root = document.getElementById("root") as HTMLDivElement;
       if (theme === "dark") {
         document.documentElement.setAttribute("data-theme", "dark");
+        root.style.colorScheme = "dark";
         setLastUserThemeMode("dark");
         set(() => ({
           userTheme: { mode: "dark", theme: "dark" },
@@ -22,6 +24,7 @@ function themeStore(set: StoreSet) {
       }
       if (theme === "light") {
         document.documentElement.setAttribute("data-theme", "light");
+        root.style.colorScheme = "light";
         setLastUserThemeMode("light");
         set(() => ({
           userTheme: { mode: "light", theme: "light" },
@@ -30,6 +33,7 @@ function themeStore(set: StoreSet) {
       }
       const defaultMode = getDefaultTheme();
       document.documentElement.setAttribute("data-theme", defaultMode);
+      root.style.colorScheme = defaultMode;
       setLastUserThemeMode("default");
       set(() => ({
         userTheme: { mode: "default", theme: defaultMode },
